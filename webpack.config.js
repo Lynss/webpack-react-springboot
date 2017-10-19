ff36f50e47c424e01f4c7497a39fac48fe8e7a9e
龙云 <ly1169134156@163.com>
2017-10-19 16:57:45 +0800

    龙云@171019 change the build path of the webpack project

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const HtmlwebpackPlugin = require('html-webpack-plugin')
 const ROOT_PATH = path.resolve(__dirname)
 const APP_PATH = path.resolve(ROOT_PATH, 'app')
 const TEM_PATH = path.resolve(APP_PATH, 'templates')
-const BUILD_PATH = path.resolve(ROOT_PATH, 'build')
+const BUILD_PATH = path.resolve(ROOT_PATH, 'src/main/resources')
 
 module.exports = {
     //项目的文件夹 可以直接用文件夹名称 默认会找index.js 也可以确定是哪个文件名字
@@ -16,7 +16,7 @@ module.exports = {
     //输出的文件名 合并以后的js会命名为bundle.js
     output: {
         path: BUILD_PATH,
-        filename: '[name].[hash].js'
+        filename: 'static/js/[name].[hash].js'
     },
     devServer: {
         historyApiFallback: true,
@@ -59,24 +59,25 @@ module.exports = {
                 query: {
                     presets: ['es2015','react']
                 }
+
             }
         ]
     },
     //添加我们的插件 会自动生成一个html文件
     plugins: [
         //这个使用uglifyJs压缩你的js代码
-        // new webpack.optimize.UglifyJsPlugin({
-        //     minimize: true
-        // }),
+        new webpack.optimize.UglifyJsPlugin({
+            minimize: true
+        }),
         //把入口文件里面的数组打包成verdors.js
         new webpack.optimize.CommonsChunkPlugin({
             name: 'vendors',
-            filename: 'vendors.js'
+            filename: 'static/vendors/vendors.js'
         }),
         new HtmlwebpackPlugin({
             title: 'Hello World app',
             template: path.resolve(TEM_PATH, 'index.html'),
-            filename: 'index.html',
+            filename: 'templates/index.html',
             //chunks这个参数告诉插件要引用entry里面的哪几个入口
             chunks: ['app', 'vendors'],
             //要把script插入到标签里
